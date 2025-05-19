354e810ed5cb268f4caad4b15887ae5af482b05c
Chayimaa <chayimaa.8888@gmail.com>
2025-05-19 09:53:55 +0200

    first commit

--- src/main/java/calcultableau/UtilisateurTab.java
+++ src/main/java/calcultableau/UtilisateurTab.java
@@ -1,5 +1,7 @@
 package calcultableau;
 
+import java.util.regex.Pattern;
+
 // Classe représentant un utilisateur
 public class UtilisateurTab {
     private String prenom; 
@@ -7,10 +9,17 @@ public class UtilisateurTab {
     private String email;  
 
     // Constructeur pour initialiser les champs de l'utilisateur
-    public UtilisateurTab(String prenom, String nom, String email) {
+      public UtilisateurTab(String prenom, String nom, String email) {
+        if (!isValidEmail(email)) {
+            throw new IllegalArgumentException("Email non valide");
+        }
         this.prenom = prenom;
         this.nom = nom;
         this.email = email;
+    }
+
+    private boolean isValidEmail(String email) {
+        return Pattern.matches("[\\w.-]+@[\\w.-]+\\.[a-z]{2,}", email);
     }
 
     // Méthode qui construit une chaîne qui repésente l'utilisateur 
